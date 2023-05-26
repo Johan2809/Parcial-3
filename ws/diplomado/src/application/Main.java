7c139dc2a301b7459bff2a86f68c66223bfcdd58
Johan2809 <johans.palmav@uqvirtual.edu.co>
2023-05-26 12:58:22 -0500

    Parcial_3

---
+++ ws/diplomado/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import controllers.EstudianteController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/EstudianteView.fxml"));
+        Parent root = loader.load();
+
+        // Obtener el controlador y llamar al método setStage()
+        EstudianteController controller = loader.getController();
+
+        controller.setStage(primaryStage);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
